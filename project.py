6b39aee7175734adf8e1422082a91429b9db93e5
Damien Maier <91182145+damienmaier@users.noreply.github.com>
2022-08-29 01:40:24 +0200

    refactor and add method to get project elements from project

--- project.py
+++ project.py
@@ -8,13 +8,13 @@ IMAGES_DIRECTORY = "data/dicoms"
 class ProjectElement:
     def __init__(self, project: "Project", element_name: str) -> None:
         self.project = project
-        self.element_name = element_name
+        self.name = element_name
 
     def image_directory_path(self) -> pathlib.Path:
-        return self.project.images_directory() / self.element_name
+        return self.project.images_directory() / self.name
 
     def mask_file_path(self, mask_name) -> pathlib.Path:
-        return self.project.masks_directory() / mask_name / (self.element_name + ".npz")
+        return self.project.masks_directory() / mask_name / (self.name + ".npz")
 
     def rename(self, new_name: str) -> None:
         image_directory_path = self.image_directory_path()
@@ -25,19 +25,12 @@ class ProjectElement:
             if mask_file_path.exists():
                 mask_file_path.rename(mask_file_path.with_stem(new_name))
 
-        dataset_file_data = self.project.dataset_file_data()
+        project_element_names = self.project.element_names()
+        project_element_names.remove(self.name)
+        project_element_names.append(new_name)
+        self.project.set_dataset_file_element_names(project_element_names)
 
-        dataset_file_files_list = dataset_file_data["files"]
-        dataset_file_files_list.remove(self.element_name)
-        dataset_file_files_list.append(new_name)
-
-        dataset_file_group_list = next(iter(dataset_file_data["groups"].values()))
-        dataset_file_group_list.remove(self.element_name)
-        dataset_file_files_list.append(new_name)
-
-        self.project.set_dataset_file_data(dataset_file_data)
-
-        self.element_name = new_name
+        self.name = new_name
 
 
 class Project:
@@ -55,6 +48,21 @@ class Project:
 
     def mask_names(self) -> list[str]:
         return [mask_file.stem for mask_file in self.masks_directory().iterdir()]
+
+    def element_names(self) -> list[str]:
+        return self.dataset_file_data()["files"]
+
+    def elements(self) -> list[ProjectElement]:
+        return [ProjectElement(self, element_name) for element_name in self.element_names()]
+
+    def set_dataset_file_element_names(self, element_names: list[str]):
+        dataset_file_data = self.dataset_file_data()
+
+        dataset_file_data["files"] = element_names
+        groups_dict = dataset_file_data["groups"]
+        groups_dict[next(groups_dict)] = element_names
+
+        self.set_dataset_file_data(dataset_file_data)
 
     def dataset_file_data(self):
         return toml.load(self.dataset_file_path())
