6261890a7b5c76e5f67856dc214b4a521a7d0a13
Damien Maier <91182145+damienmaier@users.noreply.github.com>
2022-08-29 03:20:18 +0200

    add method to get image from project

--- project.py
+++ project.py
@@ -1,8 +1,7 @@
 import pathlib
 
+import numpy as np
 import toml
-
-IMAGES_DIRECTORY = "data/dicoms"
 
 
 class ProjectElement:
@@ -10,8 +9,19 @@ class ProjectElement:
         self.project = project
         self.name = element_name
 
+        self._image_file_data_cache = None
+
     def image_directory_path(self) -> pathlib.Path:
         return self.project.images_directory() / self.name
+
+    def image_file_data(self):
+        if self._image_file_data_cache is None:
+            self._image_file_data_cache = dict(np.load(self.image_directory_path() / "0.npz"))
+
+        return self._image_file_data_cache
+
+    def image(self):
+        return self.image_file_data()["matrix"]
 
     def mask_file_path(self, mask_name) -> pathlib.Path:
         return self.project.masks_directory() / mask_name / (self.name + ".npz")
@@ -60,7 +70,7 @@ class Project:
 
         dataset_file_data["files"] = element_names
         groups_dict = dataset_file_data["groups"]
-        groups_dict[next(groups_dict)] = element_names
+        groups_dict[next(iter(groups_dict))] = element_names
 
         self.set_dataset_file_data(dataset_file_data)
 
@@ -70,4 +80,3 @@ class Project:
     def set_dataset_file_data(self, dataset_file_data: dict):
         with self.dataset_file_path().open("w") as dataset_file_descriptor:
             toml.dump(dataset_file_data, dataset_file_descriptor)
-
